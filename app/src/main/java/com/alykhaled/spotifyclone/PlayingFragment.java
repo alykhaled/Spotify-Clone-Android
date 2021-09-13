23c922f3cc72d5fe712f7f2e8f955ed870ea9c79
Aly Khaled <alykhaled2001@live.com>
2021-09-13 08:42:39 +0200

    Finish Artist Page

---
+++ app/src/main/java/com/alykhaled/spotifyclone/PlayingFragment.java
@@ -0,0 +1,38 @@
+package com.alykhaled.spotifyclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayingFragment extends Fragment {
+
+    public PlayingFragment() {
+        // Required empty public constructor
+    }
+
+    public static PlayingFragment newInstance(String param1, String param2) {
+        PlayingFragment fragment = new PlayingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_playing, container, false);
+    }
+}
